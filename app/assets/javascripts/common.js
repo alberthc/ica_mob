5ab083e0bd7f064bc555035e5adc4164fe86ebf1
Jack Cao <jackwcao@gmail.com>
2014-08-01 17:54:06 -0700

    Edit two
    add new campus page, new leaders page.

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,30 @@
+$(function(){ $(document).foundation(); });
+
+//Slow down accordion movement
+$(".accordion dd").on("click", "a:eq(0)", function (event)
+      {
+        var dd_parent = $(this).parent();
+
+        if(dd_parent.hasClass('active'))
+          $(".accordion dd div.content:visible").slideToggle("slow");
+        else
+        {
+          $(".accordion dd div.content:visible").slideToggle("slow");
+          $(this).parent().find(".content").slideToggle("slow");
+        }
+      });
+
+/*$(document).foundation();
+$(".accordion").on("click", "dd:not(.active)", function (original) {
+original = $(this);
+  $("dd.active").find(".content").slideUp("800", function(){
+
+  });
+  $(this).find(".content").slideToggle("slow", function() {
+    $('html,body').animate({
+          scrollTop: original.offset().top
+        }, 1000);
+        original.addClass("active");
+        $(".accordion dd.active").not(original).removeClass("active");
+  });
+  });*/
